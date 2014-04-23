294a10c0cb95face5cf2a37cf012681e842f72e5
Jason Hettmansperger <jjbeanz@Jasons-MacBook-Pro-7.local>
2014-04-23 09:53:23 -0400

    Added comments, changed Key generation types to match.

--- client.js
+++ client.js
@@ -1,41 +1,73 @@
+/* net library is used for nodejs sockets */
 const net = require("net");
+/* cryptography library we used for nodejs, can find documentation at
+   http://nodejs.org/api/crypto.html
+ */
 var crypto = require("crypto");
 
+/* Message to send to the server */
 var poem = 'If you can keep your head when all about you, Are losing theirs and blaming it on you, If you can trust yourself when all men doubt you, But make allowance for their doubting too; If you can wait and not be tired by waiting, Or being lied about, don’t deal in lies, Or being hated, don’t give way to hating, And yet don’t look too good, nor talk too wise.';
 
-// Create a socket (client) that connects to the server
+/* Create a socket (client) that connects to the server */
 var socket = new net.Socket();
 
+/* On initiating the client it computes its keys */
 var bob = crypto.getDiffieHellman('modp5');
 bob.generateKeys('hex');
-//collect pulbic key to send
+
+/* collect pulbic key to send */
 var clientPublicKey = bob.getPublicKey('hex');
-//holder for clients public key
+
+/* holder for clients public key */
 var serverPublicKey;
 
+/* holder for common secret */
 var commonSecret;
 
+/* Connection will prompt the server to create its keys
+   and send the client its public key.
+ */
 socket.connect(61337, "localhost", function () {
     console.log("Client: Connected to server");
 });
 
-// Let's handle the data we get from the server
+/* The server will respond with its public key. The client
+   uses this to computer the common secret. We use that as the
+   key for the cipher. Once we have computed the cipher we send
+   the clients public key and the encrypted message back to the
+   server
+  */
 socket.on("data", function (data) {
-    //console.log("Response from server: %s", data);
+
+    data = JSON.parse(data);
+    data = data.key;
+    console.log("Generated Client Public Key: \n%s", clientPublicKey);
+
+    console.log("\nReceived Server's Public Key: \n%s", data);
     serverPublicKey = data;
 
-    //Computer Bob's secret
+    /* Compute common secret */
     commonSecret = bob.computeSecret(serverPublicKey, 'hex', 'hex');
+    console.log("\nComputed Common Secret: \n%s", commonSecret);
 
-
+    /* Use the common secret as the key for the cipher */
     var cipher = crypto.createCipher('aes-256-cbc', commonSecret);
+    /* Set the cipher to auto padding */
+    cipher.setAutoPadding(auto_padding=true);
     var encryptedMsg = cipher.update(poem, 'utf8', 'hex');
     encryptedMsg += cipher.final('hex');
 
-    console.log(commonSecret);
-    console.log(encryptedMsg);
-    // Respond back
+    console.log("\nEncrypting Message: \n%s", poem);
+    console.log("\nEncrypted Secret Message: \n%s", encryptedMsg);
+
+    /* We then construct a javascript object containing the encrypted
+       message and the clients public key and send that object back to
+       the server.
+     */
+    console.log("\nSending Encrypted Message.");
     socket.write(JSON.stringify({msg: encryptedMsg, key: clientPublicKey}));
-    // Close the connection
+
+    /* Close the connection */
+    console.log("Message Sent. Closing Connection.");
     socket.end();
 });
--- server.js
+++ server.js
@@ -1,20 +1,67 @@
+/* net library is used for nodejs sockets */
 const net = require("net");
+/* cryptography library we used for nodejs, can find documentation at
+   http://nodejs.org/api/crypto.html
+ */
 var crypto = require("crypto");
 
 // Create a simple server
 var server = net.createServer(function (conn) {
+
+    /* Log when a client is connected */
     console.log("Server: Client connected");
 
-    //Generate local keys
+    /* When a user connects Generate local keys and compute the
+       servers public to pass to the client.
+     */
     var alice = crypto.getDiffieHellman('modp5');
     alice.generateKeys();
     //collect pulbic key to send
-    var serverPublicKey = alice.getPublicKey();
+    var serverPublicKey = alice.getPublicKey('hex');
+    console.log()
     //holder for clients public key
     var clientPublicKey;
     var commonSecret;
 
-    // If connection is closed
+    console.log("\nGenerated Server Public Key: \n%s", serverPublicKey);
+    /* Once the key is generated and server's public key
+       is computed, we send that key to the client so they
+       can compute the common secret key for encryption
+     */
+    console.log("\nSending Server Public Key.");
+    conn.write(JSON.stringify({key: serverPublicKey}));
+
+    /* Once the client has recieved the server's public key
+       and computed the common secret, it is used as the key
+       for the AES cipher encryption. The client then sends
+       us their public key and the encrypted message.
+     */
+    conn.on("data", function(data) {
+        data = JSON.parse(data);
+
+        clientPublicKey = data.key;
+        console.log("\nReceived Client's Public Key: \n%s", data.key);
+
+        /* Compute the common secret, requires client's public key */
+        commonSecret = alice.computeSecret(clientPublicKey, 'hex', 'hex');
+        console.log("\nComputed Common Secret: \n%s", commonSecret);
+
+        console.log("\nReceived Encrypted Message: \n%s", data.msg);
+
+        /* With the common secret computed we can use that as the
+           key for decryption of the cipher.
+         */
+        console.log("\nDecrypting Message.");
+        var decipher = crypto.createDecipher('aes-256-cbc', commonSecret);
+        var decryptedMsg = decipher.update(data.msg, 'hex', 'utf8');
+        decryptedMsg += decipher.final('utf8');
+
+        /* Print out the decrypted message */
+        console.log("Decrypted Message: \n%s",decryptedMsg);
+
+    });
+
+    /* Once the client is finished it will close the server */
     conn.on("end", function(data) {
         console.log('Server: Client disconnected');
         // Close the server
@@ -23,30 +70,10 @@ var server = net.createServer(function (conn) {
         process.exit(0);
     });
 
-    // Handle data from client
-    conn.on("data", function(data) {
-        data = JSON.parse(data);
 
-        clientPublicKey = data.key;
-        //Compute Alice's secret
-        commonSecret = alice.computeSecret(clientPublicKey, 'hex', 'hex');
-
-        console.log(commonSecret);
-        console.log(data.msg);
-        //Decipher the AES data;
-        var decipher = crypto.createDecipher('aes-256-cbc', commonSecret);
-        var decryptedMsg = decipher.update(data.msg, 'hex', 'utf8');
-        decryptedMsg += decipher.final('utf8');
-
-        console.log(decryptedMsg);
-
-    });
-
-    // Let's response with a hello message
-    conn.write(serverPublicKey);
 });
 
-// Listen for connections
+/* Listen for connections */
 server.listen(61337, "localhost", function () {
     console.log("Server: Listening");
 });
